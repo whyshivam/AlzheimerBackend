79690b00088a5957ee0b9f29109b8e1dc3dc6f15
whyshivam <shivamkr.vatsa@gmail.com>
2022-02-27 01:25:48 +0530

    basic structure created

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .auth import auth as auth_blueprint
-from .main import main as main_blueprint
-
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
@@ -17,10 +14,12 @@ def create_app():
     db.init_app(app)
 
     # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
 
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
 
     app.register_blueprint(main_blueprint)
 
